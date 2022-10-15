3c4946ab46f03a897d686cf756fd51d2cdad5e14
DP3_Andreas Manatar Lumban Gaol <114575901+andreasmlbngaol@users.noreply.github.com>
2022-10-15 12:13:49 +0700

    Tugas1

--- [DP3][Tugas1] - [3][s.code] - [Andreas Manatar Lumban Gaol][067].pas
+++ [DP3][Tugas1] - [3][s.code] - [Andreas Manatar Lumban Gaol][067].pas
@@ -1,30 +1,19 @@
 program Tugas1_Soal_3;
-
 uses crt;
-
 var
-    
     huruf : string;
     u_huruf : string;
 
-
 begin
-
     clrscr;
     write ('Masukkan huruf : '); readln(huruf);
     u_huruf := upcase (huruf);
-
     writeln ('---------------------------');
-
     case u_huruf of
-    'A','I','U','E','O' : writeln (huruf, ' merupakan huruf vokal');         'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z' : writeln (huruf, ' merupakan huruf konsonan');
-
+    'A','I','U','E','O' : writeln (huruf, ' merupakan huruf vokal');
+    'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z' : writeln (huruf, ' merupakan huruf konsonan');
     else
-
         writeln (huruf, ' bukan merupakan huruf')
-
     end;
-
     readln;
-
 end.
